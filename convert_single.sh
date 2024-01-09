c76d7829d2f7f39a765d7509bd2d289554735b90
Patricio Reinoso <patricio.reinoso.mendoza@gmail.com>
2024-01-08 23:16:26 -0500

    Create convert_single.sh

---
+++ convert_single.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+base_out="pdf_out"
+mkdir -p "$base_out"
+file="$1"
+file_out="$file"_tmp
+gs -sOutputFile="$file_out" -sDEVICE=pdfwrite -sColorConversionStrategy=Gray -dProcessColorModel=/DeviceGray -dCompatibiltyLevel=1.4 -dNOPAUSE -dBATCH "$file"
+dir_structure=$(dirname "$file")
+newdir="$base_out"/"$dir_structure"
+mkdir -p "$newdir"
+mv "$file_out" ./pdfgris/"$file"
